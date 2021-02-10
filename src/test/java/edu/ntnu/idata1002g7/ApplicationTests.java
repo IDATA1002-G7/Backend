a134e4fe2a4639ac584e63e374e6f3cd9ed5b305
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2021-02-10 09:55:17 +0100

    rename application test class

--- src/test/java/edu/ntnu/idata1002g7/ApplicationTests.java
+++ src/test/java/edu/ntnu/idata1002g7/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Idata1002g7ApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
